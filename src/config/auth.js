6435ce170b953e6ac7469b003d5a2246600363f0
Heitor Pacheco <heeitorpacheco@gmail.com>
2019-07-03 17:48:53 -0300

    Criação do projeto, junto com cadastro de usuário, alteração, e login

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'd4349e9b351e59cf2db95a1275539299',
+  expiresIn: '7d',
+};
---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'pg2019',
+  database: 'gobarber',
+  define: {
+    timestamp: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
