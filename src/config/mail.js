e5522bf0bafae8ddabdf29f2d1f58c3e45017508
Heitor Pacheco <heeitorpacheco@gmail.com>
2019-07-10 10:52:14 -0300

    Envio de email com nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'be67c46dc4d846',
+    pass: '63b4de84a11f56',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com',
+  },
+};
